226302f8cfde1ca1d654d3d174baae44840ed229
Will Currie <will@currie.id.au>
2017-04-27 07:29:26 +1000

    Use a generator to read events

--- group.py
+++ group.py
@@ -2,38 +2,49 @@ import json
 import re
 from collections import defaultdict
 
-# group CloudTrail events by invoking ARN and required iam permission
 
-events_by_arn = defaultdict(list)
+def events_from_file(filename):
+    """read previously downloaded events"""
+    with open(filename) as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            event_wrapper = json.loads(line)
+            yield json.loads(event_wrapper['message'])
 
-with open('events.json') as f:
-    while True:
-        line = f.readline()
-        if not line:
-            break
 
-        eventWrapper = json.loads(line)
-        event = json.loads(eventWrapper['message'])
-        if event['userIdentity']['type'] == 'AWSService':
-            arn = event['requestParameters']['roleArn']
-        elif event['userIdentity']['type'] == 'AWSAccount':
-            arn = event['requestParameters']['roleArn']
-        elif event['userIdentity']['type'] == 'IAMUser':
-            arn = event['userIdentity']['arn']
-        elif event['userIdentity']['type'] == 'AssumedRole':
-            arn = event['userIdentity']['sessionContext']['sessionIssuer']['arn']
-        else:
-            raise "Dunno about {0}".format(line)
-        eventName = event['eventName']
-        eventSource = re.sub(r'([^.]+).*', '\\1', event['eventSource'])
+def process(filename):
+    """group CloudTrail events by invoking ARN and required iam permission"""
+    events_by_arn = defaultdict(list)
+    for event in events_from_file(filename):
+        arn = invoker_arn(event)
+        event_name = event['eventName']
+        event_source = re.sub(r'([^.]+).*', '\\1', event['eventSource'])  # drop amazonaws.com suffix
+        events_by_arn[arn].append(event_source + ":" + event_name)
 
-        events_by_arn[arn].append(eventSource + ":" + eventName)
+    for arn, events in events_by_arn.items():
+        counts_by_event = {}
+        for event in events:
+            counts_by_event[event] = counts_by_event.get(event, 0) + 1
 
-for arn, events in events_by_arn.items():
-    counts_by_event = {}
-    for event in events:
-        counts_by_event[event] = counts_by_event.get(event, 0) + 1
+        print(arn)
+        for event, count in counts_by_event.items():
+            print("  {0} {1}".format(event, count))
 
-    print(arn)
-    for event, count in counts_by_event.items():
-        print("  {0} {1}".format(event, count))
+
+def invoker_arn(event):
+    if event['userIdentity']['type'] == 'AWSService':
+        arn = event['requestParameters']['roleArn']
+    elif event['userIdentity']['type'] == 'AWSAccount':
+        arn = event['requestParameters']['roleArn']
+    elif event['userIdentity']['type'] == 'IAMUser':
+        arn = event['userIdentity']['arn']
+    elif event['userIdentity']['type'] == 'AssumedRole':
+        arn = event['userIdentity']['sessionContext']['sessionIssuer']['arn']
+    else:
+        raise "Dunno about {0}".format(str(event))
+    return arn
+
+
+process('events.json')
